423de678e17b0a4469e8b88856d2507decdbf3d8
Amandeep0707 <amandeep190200@gmail.com>
2025-06-02 14:18:16 +0530

    Added Unreal Files

---
+++ Source/TerrainTraceEffect/TerrainTraceEffectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TerrainTraceEffectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATerrainTraceEffectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATerrainTraceEffectGameMode();
+};
+
+
+
